9877f9456f2c3023ae50d5e464d7ea2fef2df3ce
stylianos <avramids@gmail.com>
2018-05-21 15:24:08 +0100

    Pushing the Game for Unity
    This is me pushing the 2d-co_op game

---
+++ 2d_co_op/Assets/EnemySpawner.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class script : MonoBehaviour {
+
+    public GameObject PrefabToSpawn;
+    public bool is_active;
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (is_active == true) {
+
+        }
+	}
+}
