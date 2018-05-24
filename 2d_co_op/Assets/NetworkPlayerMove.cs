f3260309fd3953c8753f30992b2b9f4c9d34c49a
Stelios Avramidis <avramids@gmail.com>
2018-05-24 17:12:13 +0100

    Fixing the axis for the player
    Now, network player moves, I still don't know what's wrong with the local player.

---
+++ 2d_co_op/Assets/NetworkPlayerMove.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using UnityEngine.Networking;
+
+public class NetworkPlayerMove : NetworkBehaviour
+{
+    void Update()
+    {
+        
+        if (!isLocalPlayer)
+        {
+            Debug.Log("This is not the local player!");
+            return;
+        }
+    
+
+        var x = Input.GetAxis("Horizontal_P1") * 0.1f;
+        Debug.Log("This is the x " + x);
+        var z = Input.GetAxis("Vertical_P1") * 0.1f;
+        Debug.Log("I am moving the palyer");
+        transform.Translate(x, 0, z);
+    }
+}
